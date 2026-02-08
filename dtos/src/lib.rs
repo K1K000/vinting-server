0477e80adf5d152d77fdc0a29980f89e8bd1ecdc
SpawnCycle <joloman386@gmail.com>
2026-02-08 17:53:38 +0100

    more entity changes

--- dtos/src/lib.rs
+++ dtos/src/lib.rs
@@ -20,6 +20,12 @@ pub mod user;
 macro_rules! from_models {
     // `$entity` has to be an already imported module
     ($entity:ident, $type:ty, $vname:ident, $def:block) => {
+        impl From<$entity::Model> for $type {
+            fn from($vname: $entity::Model) -> Self {
+                $def
+            }
+        }
+
         impl From<$entity::ModelEx> for $type {
             fn from($vname: $entity::ModelEx) -> Self {
                 $def
